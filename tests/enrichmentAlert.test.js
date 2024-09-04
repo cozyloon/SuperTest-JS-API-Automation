a2f9d70848b50b3514c7b94cb5475bb1e141561a
Chathumal Sangeeth <38062287+cozyloon@users.noreply.github.com>
2024-09-04 11:03:56 +0530

    Update enrichmentAlert.test.js

--- tests/enrichmentAlert.test.js
+++ tests/enrichmentAlert.test.js
@@ -2,11 +2,21 @@ const request = require('supertest');
 const dotenv = require('dotenv'); 
 const { getAuthToken } = require('../util/authHelper');
 const enrichmentAlertPayload = require("../payloads/EnrichmentAlert.json");
+const {
+    deleteRuleById,
+    deleteRecipientById,
+    deleteEnrichmentAlertById,
+    updateAlertStatus,
+    deleteParentAlertById,
+    testConnection,
+    closePool
+} = require('../util/dbHelper');
 
 describe('Enrichment Alert Creation', () => {
     let authToken;
     let patBaseurl;
-
+    let alertId;
+    
     beforeAll(async () => { 
         const env = process.env.ENV || 'sso';
         dotenv.config({ path: `.env.${env}` });
@@ -21,5 +31,21 @@ describe('Enrichment Alert Creation', () => {
             .set('Authorization', `Bearer ${authToken}`)
             .send(enrichmentAlertPayload)
             .expect(201);
+   const locationHeader = res.headers['location'];
+        alertId = locationHeader.split('/').pop();
+    });
+
+    it.skip('remove alert from the DB', async () => {
+        if (alertId) {
+            await testConnection();
+            await updateAlertStatus(alertId);
+            await deleteEnrichmentAlertById(alertId);
+            await deleteRuleById(alertId);
+            await deleteRecipientById(alertId);
+            await deleteParentAlertById(alertId);
+            await closePool();
+        } else {
+            throw new Error('Alert ID is not available.');
+        }
     });
 });
